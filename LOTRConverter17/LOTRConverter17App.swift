a54f0b77bf1b4e199e670ef35b6cfdb258bd0632
rooshimadethis <rooshipatidar@gmail.com>
2024-05-19 18:29:29 -0400

    Initial Commit

---
+++ LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by Rooshi Patidar on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
